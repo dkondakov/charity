8c266661d7d28358164ba4e5bea0436a6131a857
Denis Kondakov <denis.n.kondakov@gmail.com>
2016-12-15 00:31:44 +0200

    [dev] Add marker with description.

--- src/client/app/components/app-map/app.map.controller.js
+++ src/client/app/components/app-map/app.map.controller.js
@@ -1,52 +1,33 @@
 import leaflet from 'leaflet';
+import config from '../../config';
+
+'use strict';
 
 class AppMapController {
   constructor() {
     const $ctrl = this;
-  
-	$ctrl.url = 'http://google.com';
+    
 	$ctrl.myMap = leaflet
-		.map('map-id')
-		.setView([51.505, -0.09], 13);
+		.map(config.MAP_ID)
+		.setView(config.MAP_INIT_POS, config.MAP_INIT_ZOOM);
   
-	leaflet.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' +
-		'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+	leaflet.tileLayer(config.MAP_URL, {
 	  maxZoom: 18,
-	  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-	  '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-	  'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+	  attribution: config.MAP_ATTR,
 	  id: 'mapbox.streets'
 	}).addTo($ctrl.myMap);
-  
-	$ctrl.myMap.on('click', onMapClick);
-  
-	leaflet.marker([51.5, -0.09])
-		.addTo($ctrl.myMap)
-		.bindPopup("<b>Hello world!</b><br />I am a popup.")
-		.openPopup();
-  
-	leaflet.circle([51.508, -0.11], 500, {
-	  color: 'red',
-	  fillColor: '#f03',
-	  fillOpacity: 0.5
-	})
-		.addTo($ctrl.myMap)
-		.bindPopup("I am a circle.");
-  
-	leaflet.polygon([
-	  [51.509, -0.08],
-	  [51.503, -0.06],
-	  [51.51, -0.047]
-	])
-		.addTo($ctrl.myMap)
-		.bindPopup("I am a polygon.");
-  
-	function onMapClick(e) {
-	  let popup = leaflet.popup();
-	  popup
-		  .setLatLng(e.latlng)
-		  .setContent(`You clicked the map at ${e.latlng.toString()}. \n Would you like to add marker here?`)
-		  .openOn($ctrl.myMap);
+	
+	$ctrl.myMap.on('click', addMarker);
+	
+	function addMarker(e){
+	  if (confirm('Would you like to add marker here?')) {
+	    let description = prompt('Please add description here!') || e.latlng;
+	  
+		leaflet.marker(e.latlng)
+			.addTo($ctrl.myMap)
+			.bindPopup(`<b>${description}</b><br />`)
+			.openPopup();
+	  }
 	}
   }
 }
--- src/client/app/config.js
+++ src/client/app/config.js
@@ -1,7 +1,12 @@
 let config = {
-  FOOTER_URL: 'http://google.com',
+  MAP_ID: 'map-id',
   MAP_URL: 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' +
   'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw',
+  MAP_ATTR: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+  '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+  'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  MAP_INIT_POS: [50.44943, 30.53],
+  MAP_INIT_ZOOM: 13
 };
 
 export default config;

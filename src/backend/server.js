06c24cf991254ec05056baa6cb656697d173450b
Denis Kondakov <denis.n.kondakov@gmail.com>
2016-12-26 00:33:32 +0200

    [dev] Added express.

---
+++ src/backend/server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const  express = require('express'),
+	app = express(),
+	port = process.env.PORT || 3000,
+	fs = require('fs');
+
+app.get('/', function (req, res) {
+  res.send('Hello, World!')
+});
+
+fs.writeFile('./src/public/markers.json', '{"marker": ' +
+	'{"id": "1", "latlng": "50.44943,30.53", "description": "Description"}}', function (err) {
+  if (err) return console.log(err);
+  console.log('Hello World > helloworld.txt');
+});
+
+app.listen(port, function () {
+  console.log('Listening on port ', port)
+});
